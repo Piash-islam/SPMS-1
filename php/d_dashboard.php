9c185f6e3a22e9edbef10dd874c6e3eafafecbc5
Adnan Rahman <arahman4946@gmail.com>
2021-09-13 10:33:44 +0600

    New Edit

--- php/d_dashboard.php
+++ php/d_dashboard.php
@@ -17,4 +17,5 @@
 
     $query = "SELECT COUNT(course.id) as 'total' FROM school LEFT JOIN department ON school.id = department.school_id LEFT JOIN program ON department.id = program.department_id LEFT JOIN course ON program.id = course.program_id WHERE school.id=$school_id";
     $crs_count = $conn->query($query)->fetch_row()[0];
+
 ?>
